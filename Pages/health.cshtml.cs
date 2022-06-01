4ebec9f42150f6f4c2054606289a796247d4457d
Alfredo Pizarro Villalobos <apizarro@redhat.com>
2022-06-01 14:43:10 -0400

    New release

---
+++ Pages/health.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace myWebApp.Pages
+{
+    public class IndexModel2 : PageModel
+    {
+        private readonly ILogger<IndexModel2> _logger;
+
+        public IndexModel2(ILogger<IndexModel2> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
